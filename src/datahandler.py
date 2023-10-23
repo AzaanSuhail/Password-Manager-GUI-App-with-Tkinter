d63d260ad21f097c679600ee6553864a8da7cee5
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-23 16:07:42 +0530

    improved data handling

--- src/datahandler.py
+++ src/datahandler.py
@@ -4,5 +4,19 @@ import os
 
 def save(website, email, password):
     with open(os.path.join(metadata.DATAPATH, "data.txt"), "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
+        data = metadata.DATASEPERATOR.join([website, email, password])
+        data_file.write(data + "\n")
         return True
+
+def load():
+    try:
+        with open(os.path.join(metadata.DATAPATH, "data.txt"), "r") as data_file:
+            lines = data_file.readlines()
+            lines = [line.strip("\n") for line in lines]
+            data = []
+            for line in lines:
+                item = line.split(metadata.DATASEPERATOR)
+                data.append(item)
+            return data
+    except FileNotFoundError as _:
+        return [[]]
--- src/metadata.py
+++ src/metadata.py
@@ -6,3 +6,6 @@ RESOURCESPATH = "Resources"
 WEBSITE_PLACEHOLDER = "ww.abc.com"
 EMAIL_PLACEHOLDER = "user@email.com"
 PASSWORD_PLACEHOLDER = "welcome123"
+
+# App Data Handling
+DATASEPERATOR =  " | "  # if we change this at any point in project, all data that has been saved until then will be marked as invalid
